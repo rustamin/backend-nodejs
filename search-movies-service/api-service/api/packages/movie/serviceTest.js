3c97d68f1cd2fabc20d020beadefc2aebdaa1d60
rustamin <rustamin@payfazz.com>
2021-02-13 18:26:10 +0700

    update test

--- search-movies-service/api-service/api/packages/movie/serviceTest.js
+++ search-movies-service/api-service/api/packages/movie/serviceTest.js
@@ -2,16 +2,13 @@
 
 const sinon = require('sinon');
 const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const chaiAsPromised = require('chai-as-promised'); // TODO: delete this?
 const service = require('./service');
 
 let assert = chai.assert;
 let expect = chai.expect;
 chai.use(chaiAsPromised);
 
-/**
- * Movie Service Tests
- */
 describe('Movie Service Tests', () => {
   const sandbox = sinon.createSandbox();
 
@@ -19,15 +16,10 @@ describe('Movie Service Tests', () => {
     sandbox.restore();
   });
 
-  it('should return unauthorized when clientId 0', async () => {
-    try {
-      let result = await service.search({
-        page: 10,
-        s: 'Batman',
-      });
-
-    } catch (err) {
-      console.log('err', err);
-    }
+  it('should successfully search movie', async () => {
+    let result = await service.search({
+      page: 10,
+      s: 'Batman',
+    });
   }).timeout(5000);
 });
